4a5f8eb1a21e9b3883fe23fc4c80a7d3fb34e45b
Atul Verma <verma.av1997@gmail.com>
2018-02-04 17:58:37 +0530

    Reverse using recursion

---
+++ Spiral matrix3.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+
+void spiral(int n,int rl,int rh,int cl,int ch,int arr[][n])
+{
+    if(rl>rh || cl>ch)
+        return;
+    spiral(n,rl+1,rh-1,cl+1,ch-1,arr);
+    static int i,j;
+    if(j==0)
+        j=ch;
+    for(i=rl;i<rh;i++)
+    {
+        printf("%d ",arr[j][i]);
+    }
+    for(j=ch;j>cl;j--)
+    {
+        printf("%d ",arr[j][i]);
+    }
+    if(j<0)
+        return;
+    for(i=rh;i>=rl;i--)
+    {
+        printf("%d ",arr[j][i]);
+    }
+    cl--;
+    if(i<0)
+        return;
+    for(j=cl+1;j<=ch;j++)
+    {
+        printf("%d ",arr[j][i]);
+    }
+    rh--;
+    i--;
+}
+
+int main()
+{
+    int m=4,n=5,count=0,i,j;
+    int arr[m][n];
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            count++;
+            arr[i][j]=count;
+        }
+    }
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            printf("%2d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    spiral(n,0,n-1,0,m-1,arr);
+}
+
+
